3398a76268bd0ea728e671a8998902e4fd07b685
Andrey Chernih <andrey.chernih@gmail.com>
2015-01-03 21:45:51 -0800

    Get rid of old name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ansible-constructor_session'
+Rails.application.config.session_store :cookie_store, key: '_railsbox_session'
