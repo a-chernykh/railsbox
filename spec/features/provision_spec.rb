15cd91c027524c0a89ab150834adff5fd154eb3a
Andrey Chernih <andrey.chernih@gmail.com>
2015-01-19 22:35:45 -0800

    Start working on integration suite

--- spec/features/provision_spec.rb
+++ spec/features/provision_spec.rb
@@ -2,6 +2,9 @@ feature 'Provision boxes', js: true do
   def create_box(components)
     visit '/'
 
+    # We don't want vagrant to ask root password (required by NFS)
+    select 'VirtualBox', from: 'Share type'
+
     components.each do |component|
       case component
       when 'rbenv'
@@ -9,7 +12,13 @@ feature 'Provision boxes', js: true do
       when 'unicorn'
         choose 'nginx + unicorn'
       when 'postgresql'
+        # Default, do nothing
+      when 'redis'
         click_on I18n.t('boxes.form.add_database')
+        click_on 'Redis'
+      when 'sidekiq'
+        click_on I18n.t('boxes.form.add_worker')
+        click_on 'sidekiq'
       end
     end
 
@@ -20,7 +29,26 @@ feature 'Provision boxes', js: true do
     expect(page.response_headers['Content-Disposition']).to include 'attachment'
   end
 
+  def test(app)
+    dir = nil
+    begin
+      dir = extract_zip(page.body)
+      FileUtils.cp_r File.join(Rails.root.join('spec/fixtures', app), '.'), dir
+      Dir.chdir("#{dir}/railsbox") do
+        begin
+          rc = system('vagrant up')
+          expect(rc).to eq true
+        ensure
+          system('vagrant destroy -f')
+        end
+      end
+    ensure
+      FileUtils.remove_entry_secure dir
+    end
+  end
+
   scenario 'rbenv, unicorn, PostgreSQL, redis, sidekiq' do
-    box = create_box %w(rbenv unicorn postgresql redis sidekiq)
+    create_box %w(rbenv unicorn postgresql redis sidekiq)
+    test('testapp1')
   end
 end
--- spec/support/test_helpers/zip_contents.rb
+++ spec/support/test_helpers/zip_contents.rb
@@ -1,8 +1,28 @@
 module TestHelpers
   module ZipContents
     def zip_list_of_files(data)
-      zip = Tempfile.new 'zip_contents'
       files = []
+      process_zip_data(data) do |zipfile|
+        files = zipfile.entries.map(&:name)
+      end
+      files
+    end
+
+    # the client is reponsible for deleting dir
+    def extract_zip(data)
+      dir = Dir.mktmpdir
+      process_zip_data(data) do |zipfile|
+        zipfile.each do |f|
+         f_path = File.join(dir, f.name)
+         FileUtils.mkdir_p(File.dirname(f_path))
+         zipfile.extract(f, f_path) unless File.exist?(f_path)
+        end
+      end
+      dir
+    end
+
+    def process_zip_data(data)
+      zip = Tempfile.new 'zip_contents'
 
       begin
         zip.write data
@@ -11,13 +31,11 @@ module TestHelpers
         # Zip::File.open_buffer is buggy, that's why we need to create
         # Tempfile and work with it
         Zip::File.open(zip.path) do |zipfile|
-          files = zipfile.entries.map(&:name)
+          yield zipfile
         end
       ensure
         zip.unlink
       end
-
-      files
     end
   end
 end
