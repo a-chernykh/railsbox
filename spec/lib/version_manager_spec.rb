ca4f62f33026d8eb44966fa92573eebb6c0d204c
Andrey Chernih <andrey.chernih@gmail.com>
2015-03-15 14:36:12 -0700

    No need in this spec

--- spec/lib/version_manager_spec.rb
+++ spec/lib/version_manager_spec.rb
@@ -1,11 +1,4 @@
 RSpec.describe VersionManager do
-  describe '.current_version' do
-    it 'delegates to #current_version' do
-      allow_any_instance_of(described_class).to receive(:current_version).and_return '1.0'
-      expect(described_class.current_version).to eq '1.0'
-    end
-  end
-
   describe '#current_version' do
     it 'extracts version from the commit tag' do
       allow(subject).to receive(:git_tags).and_return(['v1.0'])
